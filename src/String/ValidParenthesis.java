3e7044a949b7390c465734b3447f58cf3659d36f
Brad <siddhantdwivedi04@gmail.com>
2025-09-10 23:13:30 +0530

    Valid Parathesis

--- src/String/ValidParenthesis.java
+++ src/String/ValidParenthesis.java
@@ -1,7 +1,5 @@
 package String;
-
 import java.util.Stack;
-
 //https://leetcode.com/problems/valid-parentheses/description/?envType=problem-list-v2&envId=string
 //Leetcode-20
 public class ValidParenthesis {
@@ -11,7 +9,7 @@ public class ValidParenthesis {
             if (str.charAt(i)=='('||str.charAt(i)=='{'||str.charAt(i)=='[')
                 stack.push(str.charAt(i));
             else {
-                if (stack.isEmpty()) return false;
+                if (stack.isEmpty()){return false;}
                 char peek=stack.pop();
                 if ((str.charAt(i)==')'&&peek!='(')||
                         (str.charAt(i)=='}'&&peek!='{')||
